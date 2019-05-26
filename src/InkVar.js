95b82f0c0bbd9346d411698edc6254b55cab303f
Rowan Cockett <rowanc1@gmail.com>
2019-05-26 17:05:32 -0600

    Compare equal if array

--- src/InkVar.js
+++ src/InkVar.js
@@ -3,6 +3,69 @@ const Format = require('d3-format');
 
 import { BaseGetProps, propDef, getProp, setProp, getPropFunction, dispatchUpdates, getIFrameFunction } from './InkDynamicProps.js';
 
+var isArrayEqual = function (value, other) {
+    // https://gomakethings.com/check-if-two-arrays-or-objects-are-equal-with-javascript/
+
+    // Get the value type
+    var type = Object.prototype.toString.call(value);
+
+    // If the two objects are not the same type, return false
+    if (type !== Object.prototype.toString.call(other)) return false;
+
+    // If items are not an object or array, return false
+    if (['[object Array]', '[object Object]'].indexOf(type) < 0) return false;
+
+    // Compare the length of the length of the two items
+    var valueLen = type === '[object Array]' ? value.length : Object.keys(value).length;
+    var otherLen = type === '[object Array]' ? other.length : Object.keys(other).length;
+    if (valueLen !== otherLen) return false;
+
+    // Compare two items
+    var compare = function (item1, item2) {
+
+        // Get the object type
+        var itemType = Object.prototype.toString.call(item1);
+
+        // If an object or array, compare recursively
+        if (['[object Array]', '[object Object]'].indexOf(itemType) >= 0) {
+            if (!isArrayEqual(item1, item2)) return false;
+        }
+
+        // Otherwise, do a simple comparison
+        else {
+
+            // If the two items are not the same type, return false
+            if (itemType !== Object.prototype.toString.call(item2)) return false;
+
+            // Else if it's a function, convert to a string and compare
+            // Otherwise, just compare
+            if (itemType === '[object Function]') {
+                if (item1.toString() !== item2.toString()) return false;
+            } else {
+                if (item1 !== item2) return false;
+            }
+
+        }
+    };
+
+    // Compare properties
+    if (type === '[object Array]') {
+        for (var i = 0; i < valueLen; i++) {
+            if (compare(value[i], other[i]) === false) return false;
+        }
+    } else {
+        for (var key in value) {
+            if (value.hasOwnProperty(key)) {
+                if (compare(value[key], other[key]) === false) return false;
+            }
+        }
+    }
+
+    // If nothing failed, return true
+    return true;
+
+};
+
 
 class InkVar extends BaseGetProps{
 
@@ -62,6 +125,9 @@ class InkVar extends BaseGetProps{
             let variable = this.store.getState().variables[this.name];
             let val = this.value;
             let same = variable.value == val;
+            if(variable.value.length !== undefined){
+                same = isArrayEqual(variable.value, val);
+            }
             let bothNaN = Number.isNaN(variable.value) && Number.isNaN(val);
             if(variable && ( same || bothNaN ) ){
                 // The variable exists and is the same, return.
